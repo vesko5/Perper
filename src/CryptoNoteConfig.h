83dc6d87f1bdf46c9667c96a19a78d6c24644e80
vesko5 <vesko@tiscali.it>
2018-01-01 01:10:30 +0100

    seed ip
    server ip for seed

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,6 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
+  "111.11.11.11:18001",
+  "222.22.22.22:18003",
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
 };
